536931ff9715cb253e5d8182c6af1ee9a6f6718b
evelynfcastro <evelynfcastro2002@gmail.com>
2024-02-05 08:59:20 -0300

    learning week 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -3,7 +3,25 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Learning05 World!");
-    }
+
+         List<Shape> shapes = new List<Shape>();
+        Square square1 = new Square("blue", 4);
+        shapes.Add(square1);
+
+        Rectangle rectangle1 = new Rectangle("pink", 4,3);
+        shapes.Add(rectangle1);
+
+        Circle circle1 = new Circle("green", 3);
+        shapes.Add(circle1);
+       
+      
+
+        foreach (Shape sh in shapes)
+        {
+            Console.WriteLine($"The {sh.GetColor()} shape has an area of {sh.GetArea()} ");
+        }
+            }
+
 }
